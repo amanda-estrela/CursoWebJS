a99bb87cd23dd971e76b6ff724c5b0fd8da89e2e
Amanda Estrela <efernandes.amanda@gmail.com>
2022-06-22 22:50:15 -0300

    First commit

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+console.log("Sentença de código.")
+
+{
+    //existe bloco vazio
+    console.log("Bloco de código agrupa sentenças de códigos.")
+    {
+        console.log("Pode agrupar blocos dentro de blocos.")
+    }
+}
+
+console.log("JavaScrip é organizado em sentenças e blocos de código, com uso opcional do ponto-vírgula para finalizar as sentenças.")
