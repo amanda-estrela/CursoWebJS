58cb53fff457c2ddb5d45c4141b92ebc7d4ac3a6
Amanda Estrela <efernandes.amanda@gmail.com>
2022-06-27 02:30:17 -0300

    Função construtora

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,33 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+    //atributo privado
+    let velocidadeAtual = 0
+
+    //metodo publico
+    this.acelerar = function () {
+        if(velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }   
+
+    //metodo publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+
+}
+
+const uno = new Carro // por ter parametro padrao, pode declarar sem os parenteses
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 74)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+
+console.log(ferrari.getVelocidadeAtual())
