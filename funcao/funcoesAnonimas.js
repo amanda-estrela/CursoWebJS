ce17183a8c2e211f9f15e126881801f3cd576acd
Amanda Estrela <efernandes.amanda@gmail.com>
2022-06-27 02:08:58 -0300

    funções anônimas e callbacks

---
+++ funcao/funcoesAnonimas.js
@@ -0,0 +1,27 @@
+function soma( a, b) {
+    return a + b
+}
+
+const imprimirResultado = function ( a, b, operacao = soma) {
+    console.log(operacao(a, b))
+}
+
+imprimirResultado(3,4)
+imprimirResultado(3,4, soma)
+imprimirResultado(3,4, function(x,y){
+    return x * y
+})
+imprimirResultado(3,4, (x, y) => x - y)
+
+
+const pessoa = {
+    falar: function () {
+        console.log('Opa')
+    }
+    /* ou
+        falar() {
+        console.log('Opa')
+    }*/
+}
+
+pessoa.falar()
