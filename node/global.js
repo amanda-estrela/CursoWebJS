c779cfb8a8e8a6e16b974b458776354d93a0f067
Amanda Estrela <efernandes.amanda@gmail.com>
2022-07-02 23:54:14 -0300

    Node

---
+++ node/global.js
@@ -0,0 +1,10 @@
+//console.log(global)
+global.MinhaApp = Object.freeze({
+    saudacao() {
+        return 'Estou em todos os lugares!'
+    },
+    nome: "Sistema Legal"
+})
+
+//evitar alterar algo no global
+
