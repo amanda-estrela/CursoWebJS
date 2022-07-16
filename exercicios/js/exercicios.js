e5f8bd34d3135cd43ab06a98298f8be4843f870d
Amanda Estrela <efernandes.amanda@gmail.com>
2022-07-15 22:37:48 -0300

    Exercícios JS

---
+++ exercicios/js/exercicios.js
@@ -0,0 +1,44 @@
+//1
+function operacoes(x, y) {
+    let a = x
+    let b = y
+    console.log(`
+    Soma: ${a+b}
+    Subtração: ${a-b}
+    Multiplicação: ${a*b}
+    Divisão: ${a/b}`)
+}
+
+//operacoes(15, 3)
+
+//2
+function tipoTriangulo(a, b, c) {
+    if(a == b && a == c) {
+        console.log("Este é um triângulo equilátero.")
+    } else if (a == b || a == c || c ==b) {
+        console.log("Este é um triângulo isósceles.")
+    } else {
+        console.log('Este é um triângulo escaleno.')
+    }
+}
+
+// tipoTriangulo(3,3,3)
+// tipoTriangulo(3,3,4)
+// tipoTriangulo(3,4,5)
+
+//3
+function expoente(base, expoente) {
+    console.log(base ** expoente)
+}
+
+// expoente(2,3)
+// expoente(5, 2)
+// expoente(3,3)
+
+//4
+function divisao(dividendo, divisor) {
+    console.log(`Resultado: ${dividendo/divisor}, Resto: ${dividendo % divisor}`)
+}
+
+divisao(16, 4)
+divisao(20, 3)
