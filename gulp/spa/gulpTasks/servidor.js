4e843e5bee365eeda480ea5a457914ebb2410103
Amanda Estrela <efernandes.amanda@gmail.com>
2022-07-18 22:59:54 -0300

    SPA com Gulp

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,13 +1,24 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
+
+function servidor(){
+    return gulp.src('build')
+        .pipe(webserver({
+            port:8080,
+            open:true,
+            livereload:true,
+        }))
+}
 
 function monitorarArquivos(cb){
+    watch('src/**/*.html', () => gulp.series('appHTML')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src/**/*.imgs', () => gulp.series('appIMG')())
+    watch('src/**/*.scss', () => gulp.series('appCSS')())
+
     return cb()
 }
-
-function servidor(cb){
-    return cb()
-}
-
 module.exports = {
     monitorarArquivos,
     servidor
