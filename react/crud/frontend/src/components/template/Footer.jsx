9fd62d9e5b1c4acd3ab6f9648237697d3a306c2e
Amanda Estrela <efernandes.amanda@gmail.com>
2022-08-12 23:58:29 -0300

    Projeto Cadastro de Cliente - CRUD

---
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className='fa fa-heart text-danger'></i> por 
+            <a href="http://www.github.com/amanda-estrela"> Amanda Estrela</a>
+        </span>
+    </footer>
